df7a7c468b8be5822b2d2593ab7ebf246bcf4faa
youwei zhong <taozhong521@hotmail.com>
2019-03-28 14:41:11 -0400

    change to city.title in line 24

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -20,10 +20,11 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('\nWhich city would you like to explore?\nChicago?\nNew York?\nWashington?\n').lower()
-        if city in ('new york', 'chicago', 'washington'):
-           break
+        if city not in ('new york', 'chicago', 'washington'):
+            print(city.title() + " is not one of the three cities")
+            continue
         else:
-            print(f'Sorry, please input "Chicago", "New York", or "Washington" only.')
+            break
 
 
 
