af703a9fc59a8944ee09205f3b930b86d11f17f5
maria <maria@ashleyadvertising.com>
2019-04-10 10:55:52 -0400

    multiple code changes

--- .gitignore file/Project 2 bikeshare.py
+++ .gitignore file/Project 2 bikeshare.py
@@ -3,38 +3,31 @@ import pandas as pd
 import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }          
+              'washington': 'washington.csv' }
 filename = 'chicago.csv','new_york_city.csv','washington.csv'
 print('Hello! Let/s explore some US bikeshare data!\n')
-city = [] 
-list1 = ('chicago','new york city', 'washington') 
-# request user input
-answer_city = input ('What City would you like to analyze Bikeshare data for? Enter city\n')
-while answer_city.lower() not in list1:
-    if answer_city.lower() == 'chicago':
-     city = ['chicago']
-     pass
-    elif answer_city.lower() == 'new york city':
-     city = ['new_york_city']
-     pass
-    elif answer_city.lower() == 'washington':
-     city = ['washington']
-    else: input('you have entered an invalid response. please enter a city on the list\n')
-    while True: 
-        if answer_city.lower() not in l:
-         input('\nWould you like to see data for chicago, new York City, or washington?\n') 
-         break
-            
+while True:
+    try:
+        city = input ('What City would you like to analyze Bikeshare data for? Enter city\n')
+        city = city.lower()
+        if city in CITY_DATA[city]:
+            print ("\n{} if true,if not restart now \n".format(city))
+            break
+    except (ValueError,KeyError):
+        print("Please pick city on list")
+
+
+
 month = ('january','february','march','april','may','june')
 list2 = ('january','february','march','april','may','june')
 answer_month = input('What month would you like to analyze Bikeshare data for? Enter month\n')
-while answer_month.lower() not in list2:             
+while answer_month.lower() not in list2:
           if answer_month.lower() == 'january':
            month = ['january']
           elif answer_month.lower() == 'february':
            month = ['february']
           elif answer_month.lower() == 'march':
-           month = ['march']           
+           month = ['march']
           elif answer_month.lower() == 'april':
            month = ['april']
           elif answer_month.lower() == 'may':
@@ -43,19 +36,19 @@ while answer_month.lower() not in list2:
            month = ['june']
           elif answer_month.lower() == 'july':
            month = ['july']
-          else: input('\nYou have entered an invalid response. Would you like to restart? Enter yes or no.\n') 
+          else: input('\nYou have entered an invalid response. Would you like to restart? Enter yes or no.\n')
           break
 
-days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'] 
+days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 list3 = ('monday','tuesday','wednesday','thursday','friday','saturday','sunday')
 answer_day = input('What day of the week would you like to analyze Bikeshare data for? Enter day\n')
-while answer_day.lower() not in list3:             
+while answer_day.lower() not in list3:
           if answer_day.lower() == 'monday':
            day = ['monday']
           elif answer_day.lower() == 'tuesday':
            day = ['tuesday']
           elif answer_day.lower() == 'wednesday':
-           month = ['wednesday']           
+           month = ['wednesday']
           elif answer_day.lower() == 'thursday':
            day = ['thursday']
           elif answer_day.lower() == 'friday':
@@ -66,11 +59,11 @@ while answer_day.lower() not in list3:
            day = ['sunday']
           else: input('\nYou have entered an invalid response. Would you like to restart? Enter yes or no.\n')
           break
-        
+
 usertype = ('customer','subscriber')
 list4 = ('customer', 'subscriber')
 answer_usertype = input('What user type would you like to analyze Bikeshare data for? Enter user type\n')
-while answer_usertype.lower() not in list4:  
+while answer_usertype.lower() not in list4:
            if answer_usertype.lower() == 'customer':
             usertype = ['customer']
            elif answer_usertype.lower() == 'subscriber':
@@ -78,20 +71,20 @@ while answer_usertype.lower() not in list4:
            else: input('\nYou have entered an invalid response. Would you like to restart? Enter yes or no.\n')
 # load data file into a dataframe
 if answer_city == 'chicago':
- 
+
  df = pd.read_csv('chicago.csv')
 
 elif answer_city == 'new york city':
- 
- df = pd.read_csv('new_york_city.csv') 
+
+ df = pd.read_csv('new_york_city.csv')
 
 elif answer_city == 'washington':
- 
+
  df = pd.read_csv('washington.csv')
 
 
 df['Start Time'] = pd.to_datetime(df['Start Time'])
-# extract hour from the Start Time 
+# extract hour from the Start Time
 df['hour'] = df['Start Time'].dt.hour
 # most popular hour
 popular_hour = df['hour'].mode()[0]
@@ -99,7 +92,7 @@ print('You have chosen to view the following Bikesahre data, you wanted to see',
 print()
 print('For', answer_city, 'Most Popular Start Hour:', popular_hour)
 
-print('the following is all six months of data for the city you selected above. we will show total users and trips by point of origin, in descending order.\n') 
+print('the following is all six months of data for the city you selected above. we will show total users and trips by point of origin, in descending order.\n')
  # print counts for each user type
 user_types = df['User Type'].value_counts()
 
