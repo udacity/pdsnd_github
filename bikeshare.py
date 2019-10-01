0f3f3f3ba3c97e6afd7b867d93d889feaef5c841
Sulaiman Idris <connect2sulaiman@gmail.com>
2019-10-01 13:59:52 +0100

    ref: rearranged data filters variables

--- bikeshare.py
+++ bikeshare.py
@@ -16,9 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    city = ''
-    month =''
-    day = ''
+    city = month = day = ''
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = str(input('Would you like to see data for Chicago, New York city, or Washington? \n')).lower()
     while city not in ['chicago','new york city','washington']:
