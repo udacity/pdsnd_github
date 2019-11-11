089ee4e9e35a8e33015243a203b82d8ec95b12dc
Naga R Pramod Padakandla <pnrpramod@Nagas-MacBook-Pro.local>
2019-11-10 22:35:43 -0600

    added additional lines to the python script

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+##Adding Data
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
