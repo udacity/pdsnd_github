3eea4efb2f64a17af3f0d0a52e806147c62bd598
BP <benny.peeler@gmail.com>
2019-11-17 16:45:00 -0600

    Expanded opening comment to user in py file

--- bikeshare.py
+++ bikeshare.py
@@ -66,7 +66,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print ( 'Hello! Let\'s explore some US bikeshare data!' )
+    print ( 'Hello and Good Day! Let\'s explore some US bikeshare data!' )
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = valid_city[ get_selection ( cnum , valid_city ) - 1 ]
     city_df = pd.read_csv ( CITY_DATA[ city ] )
