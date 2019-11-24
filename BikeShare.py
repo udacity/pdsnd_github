47b48c0adca9acef69bc8cec4ef3cd70308a6bf2
AbdulmalikAK <58115789+AbdulmalikAK@users.noreply.github.com>
2019-11-25 00:01:11 +0300

    Update BikeShare.py

--- BikeShare.py
+++ BikeShare.py
@@ -1,1 +1,19 @@
 
+ # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+ while True :
+    day = raw_input("\n Enter name of the  day\n Monday,\n Tuesday,\n Wednesday,\n Thursday,"
+            "\n Friday,\n Saturday,\n Sunday\n of week to filter by, or \"all\" to apply no day filter\n").lower()
+        if day == "monday"
+         or day =="tuesday"
+         or day =="wednesday"
+         or day =="thursday"
+         or day =="friday"
+         or day =="saturday"
+         or day =="sunday"
+         or day =="all" 
+     break:
+   else:
+    print('Invalid input! . Plase try again :) ')
+    
+    print('-'*40)
+    return city, month, day 
