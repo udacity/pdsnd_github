ab8663b286f6a31b4b10c71974871d3d2398b608
valaanele <35336110+valaanele@users.noreply.github.com>
2019-12-31 12:49:23 +0200

    Update Anele Project Submission.py

--- Anele Project Submission.py
+++ Anele Project Submission.py
@@ -50,6 +50,17 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
 
     return df
 
