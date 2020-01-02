e5cd13a00df3438fc74845a6439f52faef1ffbba
valaanele <35336110+valaanele@users.noreply.github.com>
2020-01-02 08:28:33 +0200

    Update Anele Project Submission.py

--- Anele Project Submission.py
+++ Anele Project Submission.py
@@ -3,6 +3,8 @@ Explore US Bikeshare Data
 Use Python to understand U.S. bikeshare data. Calculate statistics and build an interactive environment where a user chooses the datata and filter for a dataset to analyze
 
 
+change to second branch
+
 :::::::GUIDELINES:::::::::
 
 import time
