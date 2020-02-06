2561bea25e4339fa5fd9dab413858df9c51c4ba1
Abdullah Alnassar <afalnassar@gmail.com>
2020-02-06 23:30:34 +0300

    reducing the number of lines for CITY_DATA

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,8 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
+CITY_DATA = { 'chicago': 'chicago.csv', 'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
