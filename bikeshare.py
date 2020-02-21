598292496e594d080f70904388e54a96d441ed3b
Henry Ng <jupafly@hotmail.com>
2020-02-21 11:34:35 -0700

    Update bikeshare.py
    Added some verbiage to correctly reflect what the function was supposed to do.

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
 
-    This function should also require invalid inputs
+    This function should also require invalid inputs to be handled correctly and not stop the code
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
