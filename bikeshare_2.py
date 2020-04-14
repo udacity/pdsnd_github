3f0ac36301af34243e3e508f356075558902d7e9
Bradley Cao <hungcao1995@gmail.com>
2020-04-14 15:33:18 -0400

    Refactor Code in Station Stats outputs

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -110,16 +110,16 @@ def station_stats(df):
 
     # display most commonly used start station
     popular_start_station = df['Start Station'].mode()[0]
-    print("Most Popular Start Station is ", popular_start_station)
+    print("Most Popular Start Station is {}".format(popular_start_station))
 
     # display most commonly used end station
     popular_end_station = df['End Station'].mode()[0]
-    print("Most Popular End Station is ", popular_end_station)
+    print("Most Popular End Station is {}".format(popular_end_station))
 
     # display most frequent combination of start station and end station trip
     df['Combined Station'] = df['Start Station'] + ' + ' + df['End Station']
     popular_combine_station = df['Combined Station'].mode()[0]
-    print("Most Popular Combined Station is ", popular_combine_station)
+    print("Most Popular Combined Station is {}".format(popular_combine_station))
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
