8c269c56753e8390675d8bcc97aed113145c1ee2
arpit282 <47824144+arpit282@users.noreply.github.com>
2020-05-02 10:51:47 +0530

    bikeshare.py and gitignore comitted

---
+++ bikeshare.py
@@ -0,0 +1,3 @@
+print("hello world")
+print(" I havent done ay python project so i dont think that its right to ask for the same as its not in the course and its not legal also to \
+      ask  for the same ")
