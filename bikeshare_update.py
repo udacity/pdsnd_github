52297a1babf5a19707e4f9f6e8d4e0074d788508
Elias Nordlinder <Elias.Nordlinder@gmail.com>
2020-05-15 08:54:15 +0200

    refactor: Changed description trip duration

--- bikeshare_update.py
+++ bikeshare_update.py
@@ -170,7 +170,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays the full statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
