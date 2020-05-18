6fd326fca2b89087beb1c561961f92a611231bbf
Tarek Khaled <contact@tarekkhaled.com>
2020-05-18 16:10:22 -0700

    Changed df['day'] code to solve AttributeError.

--- bikeshare.py
+++ bikeshare.py
@@ -72,7 +72,7 @@ def load_data(city, month, day):
 
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
-    df['day'] = df['Start Time'].dt.weekday_name
+    df['day'] = df['Start Time'].dt.day_name()
 
     # filter by month if applicable
     if month != 'all':
