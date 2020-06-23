d727e6215823a72fde98b8ec672772e8d3032268
Alexandru <alexandrucozma88@gmail.com>
2020-06-23 16:12:00 +0200

    Refactoring_branch_bikeshare_update

--- bikeshare.py
+++ bikeshare.py
@@ -197,7 +197,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         raw_data(df)
-
+        print("This is on the refactoring branch")
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
