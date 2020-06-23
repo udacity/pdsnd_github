8ff97877c5fd13c9a44d5c8542fe094559b181b2
Alexandru <alexandrucozma88@gmail.com>
2020-06-23 15:59:41 +0200

    “bikeshare.py_print_statement_added”

--- bikeshare.py
+++ bikeshare.py
@@ -197,7 +197,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         raw_data(df)
-
+        print("Awsome job! Thanks for checking in.")
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
