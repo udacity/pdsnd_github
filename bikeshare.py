0a09ba006cbcaac819c8d223d658d85640a3c246
truptipp <trupti_patil@yahoo.com>
2020-06-28 10:57:02 -0400

    docs: changes to bikeshare.py doc string
    Added specific month range in documentation
    * specific month range Jan-June

--- bikeshare.py
+++ bikeshare.py
@@ -11,7 +11,7 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze.
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) month - name of the month from Jan to Jun to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
