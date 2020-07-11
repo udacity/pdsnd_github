f72fbdf66a63f4d852f5991edefa335f36ccf16c
mv-udacity <68136103+mv-udacity@users.noreply.github.com>
2020-07-10 20:26:55 -0700

    Update mv_bikeshare_project.py
    Remove "please" from else statement

--- mv_bikeshare_project.py
+++ mv_bikeshare_project.py
@@ -30,7 +30,7 @@ def get_city():
         elif city.lower() == 'washington':
             return 'washington.csv'
         else:
-            print('Sorry, I do not understand your input. Please input either '
+            print('Sorry, I do not understand your input. Input either '
                   'Chicago, New York, or Washington.')
 
 def get_time_period():
