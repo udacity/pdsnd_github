9e427d6b80a5e4dfde24c9d8bb18fc83e6c6b611
NONTAWAT149 <nontawat149@gmail.com>
2020-07-25 16:09:28 +0100

    add respond answer of users to stop program

--- bikeshare.py
+++ bikeshare.py
@@ -207,6 +207,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('Your answer is NO, we will stop the program')
             break
 
 
