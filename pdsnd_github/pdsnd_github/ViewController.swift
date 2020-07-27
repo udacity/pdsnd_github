b977c08f40b878b5dfc6f0963e46a6aa617ceaf9
SDR <Beatricesdr@gmail.com>
2020-07-27 11:10:31 -0700

    Project 3 - created a clone of a GitHub repo

---
+++ pdsnd_github/pdsnd_github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pdsnd_github
+//
+//  Created by S DR on 7/27/20.
+//  Copyright © 2020 S DR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pdsnd_github/pdsnd_githubTests/pdsnd_githubTests.swift
@@ -0,0 +1,34 @@
+//
+//  pdsnd_githubTests.swift
+//  pdsnd_githubTests
+//
+//  Created by S DR on 7/27/20.
+//  Copyright © 2020 S DR. All rights reserved.
+//
+
+import XCTest
+@testable import pdsnd_github
+
+class pdsnd_githubTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
