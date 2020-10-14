18f8625e00d8b91eb8ce17e52c27273ab458cc7b
Patnaik, Rahul <RahulPatnaik@eaton.com>
2020-10-14 11:56:53 +0530

    change the input text message

--- BikeShare.py
+++ BikeShare.py
@@ -292,8 +292,8 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        restart = input('\nWould you like to restart? Enter y or n.\n')
+        if restart.lower() != 'y':
             break
 
 
