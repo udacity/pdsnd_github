9ecc5b9c10f7fb94fe7443338fc700f0cede7010
sam <samdovon@gmail.com>
2020-11-05 12:56:40 +0700

    refactor : Refactoring code line 5 in bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Refactoring code1 : Dataset from csv 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
