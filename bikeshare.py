981ee83321ba95adc066622dbf42ae5009e9a7eb
Sultan Alrasheedi <salrasheedijobs@gmail.com>
2020-12-23 09:48:53 +0300

    fix some splling mistakes

--- bikeshare.py
+++ bikeshare.py
@@ -58,7 +58,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Load data for the specified city and filters by month and day if applicable.
 
     Args:
         (str) city - name of the city to analyze
