ebe845b60d1e299f1c9f3d10b703ef86d90635cd
Maria Fernanda Narvaez Eraso <mfnarera@davivienda.com>
2021-01-11 05:54:52 -0500

    Add a warning againts capital letters

--- Resubmit_Bikeshare_Project.py
+++ Resubmit_Bikeshare_Project.py
@@ -36,7 +36,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!, keep in mind that you should not use capital letters')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle     invalid inputs
     
     ciudad=' '
