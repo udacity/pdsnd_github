d3f2623eee980bdfc941a2aa1312f9ec926301fc
Dan Buegeleisen <dabueg10@gmail.com>
2021-01-16 12:27:10 -0800

    add thank you prompt at end

--- bikeshare.py
+++ bikeshare.py
@@ -318,6 +318,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        print("Thank you for using my app.")
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
