2f890628be0c640ce45af08f735baa691b8dafd3
Ngbede <ngbedealike@gmail.com>
2021-05-23 07:28:26 +0100

    additional in-line comment to my bikeshare code

--- bikeshare.py
+++ bikeshare.py
@@ -53,7 +53,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
 
-
+# load bikeshare data with respect to stated city, month and day
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
